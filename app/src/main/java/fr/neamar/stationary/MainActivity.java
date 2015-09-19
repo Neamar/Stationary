bd3afa397263bd108bbc08d566be7ac53444737c
Neamar <neamar@neamar.fr>
2015-09-19 15:53:11 +0200

    Reading raw sensor data

---
+++ app/src/main/java/fr/neamar/stationary/MainActivity.java
@@ -0,0 +1,70 @@
+package fr.neamar.stationary;
+
+import android.app.Activity;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Surface;
+
+public class MainActivity extends Activity implements SensorEventListener {
+    public static final float THRESHOLD = 0.2f;
+
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
+
+    public void onCreate(Bundle state) {
+        super.onCreate(state);
+        setContentView(R.layout.activity_main);
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
+    }
+
+    protected void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_GAME);
+    }
+
+    protected void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(this);
+    }
+
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    }
+
+    public void onSensorChanged(SensorEvent event) {
+        //Right in here is where you put code to read the current sensor values and
+        //update any views you might have that are displaying the sensor information
+        //You'd get accelerometer values like this:
+        if (event.sensor.getType() != Sensor.TYPE_LINEAR_ACCELERATION)
+            return;
+
+        float mSensorX = 0;
+        float mSensorY = 0;
+
+        switch (getWindowManager().getDefaultDisplay().getRotation()) {
+            case Surface.ROTATION_0:
+                mSensorX = event.values[0];
+                mSensorY = event.values[1];
+                break;
+            case Surface.ROTATION_90:
+                mSensorX = -event.values[1];
+                mSensorY = event.values[0];
+                break;
+            case Surface.ROTATION_180:
+                mSensorX = -event.values[0];
+                mSensorY = -event.values[1];
+                break;
+            case Surface.ROTATION_270:
+                mSensorX = event.values[1];
+                mSensorY = -event.values[0];
+        }
+
+        if(mSensorX > THRESHOLD || mSensorY > THRESHOLD) {
+            Log.e("WTF", "X: " + Float.toString(mSensorX) + ". Y: " + Float.toString(mSensorY));
+        }
+    }
+}
